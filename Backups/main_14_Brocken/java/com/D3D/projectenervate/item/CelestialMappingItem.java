990c7e0b0d53e18a18b9ff18113a246fc1ca5047
dpdurczok <38078090+dpdurczok@users.noreply.github.com>
2026-06-18 22:27:44 +0200

    Remove backup files from repository

--- Backups/main_14_Brocken/java/com/D3D/projectenervate/item/CelestialMappingItem.java
+++
@@ -1,9 +0,0 @@
-package com.D3D.projectenervate.item;
-
-import net.minecraft.world.item.Item;
-
-public class CelestialMappingItem extends Item {
-    public CelestialMappingItem(Properties properties) {
-        super(properties);
-    }
-}
--- Backups/main_Backup_16/java/com/D3D/projectenervate/api/ProjectEnervateCreateContraptionEmcAccess.java
+++
@@ -1,8 +0,0 @@
-package com.D3D.projectenervate.api;
-
-import com.D3D.projectenervate.world.PlacedBlockAdaptiveEmcData;
-import java.util.Map;
-
-public interface ProjectEnervateCreateContraptionEmcAccess {
-    Map<Long, PlacedBlockAdaptiveEmcData.Entry> projectenervate$getCreateContraptionPlacedBlockEmc();
-}
